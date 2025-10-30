1037e9d92dfd7c815e2f266686acac3d05e49d10
Nrenje <aniruddhabutbetter@gmail.com>
2025-10-30 18:03:07 +0530

    progress

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -2,6 +2,7 @@ function SearchBar() {
     return (
         <div>
             <input className="inputbox" type="text" placeholder="Search any Movie!" />
+            <button className="search-btn">Search</button>
         </div>
     )
 }
